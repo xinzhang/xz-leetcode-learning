3ab6d703524ea4e8a865c287a3e3566032d50d6b
Xin Zhang <xinzhang@192-168-1-125.tpgi.com.au>
2020-02-19 19:57:26 +1100

    add 001 answer

--- leetcode/test-001.js
+++ leetcode/test-001.js
@@ -1,13 +1,26 @@
+// const twoSum = (nums, target) => {
+//     const result = {};
+//     for (let i = 0; i < nums.length; i++) {
+//         const num = nums[i];
+//         if (result[target - num] != undefined) {
+//             return [result[target - num], i];
+//         }
+//         result[num] = i;
+//     }
+// };
+
+// bruteforce
 const twoSum = (nums, target) => {
-    const result = {};
     for (let i = 0; i < nums.length; i++) {
-        const num = nums[i];
-        if (result[target - num] != undefined) {
-            return [result[target - num], i];
+        for (let j = i; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j];
+            }
         }
-        result[num] = i;
     }
 };
 
 nums = [2, 7, 11, 15];
 console.log(twoSum(nums, 9));
+console.log(twoSum(nums, 13));
+console.log(twoSum(nums, 26));
